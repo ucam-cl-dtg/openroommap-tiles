af8cfa2d5de81e9671ccb2ecaed7dfc7b574faa2
Andrew Rice <acr31@cam.ac.uk>
2013-03-03 12:10:52 +0000

    Some errors with bash, set -e and diff.  Things got complicated so I simplified and removed some of the incremental build logic.  Let's just get it to work at all first

--- src/main/scripts/make-all.sh
+++ src/main/scripts/make-all.sh
@@ -6,8 +6,9 @@ HOST=$1
 
 ssh $HOST -f -L 5433:localhost:5432 sleep 3
 touch lastupdateid.txt
-perl get-last-update-id.pl > temp.txt
-if [ `diff temp.txt lastupdateid.txt >/dev/null 2>&1`]; then
+POLLED_UPDATE=`perl get-last-update-id.pl`
+STORED_UPDATE=`cat lastupdateid.txt`
+if [ "$POLLED_UPDATE" != "$STORED_UPDATE" ]; then
     echo "Last update id has changed"
     for floor in `seq 0 1 2`; do 
 	ssh $HOST -f -L 5433:localhost:5432 sleep 3
@@ -20,15 +21,11 @@ if [ `diff temp.txt lastupdateid.txt >/dev/null 2>&1`]; then
 
     for floor in `seq 0 1 2`; do
 	ssh $HOST -f -L 5433:localhost:5432 sleep 3
-	perl generate-tiles.pl $floor people,rooms,objects,desksOnly > temp.svg
-	touch floor$floor-allocation.svg
-	if [ ! `diff floor$floor-allocation.svg temp.svg >/dev/null 2>&1`]; then 
-	    mv temp.svg floor$floor-allocation.svg
-	    inkscape floor$floor-allocation.svg -A floor$floor-allocation.pdf
-	fi
+	perl generate-tiles.pl $floor people,rooms,objects,desksOnly > floor$floor-allocation.svg
+	inkscape floor$floor-allocation.svg -A floor$floor-allocation.pdf
     done
 
-    cp temp.txt lastupdateid.txt
+    `echo $POLLED_UPDATE > lastupdateid.txt`
 fi
 
 # for floor in `seq 0 1 2`; do ./make-files floor$$floor-labels.svg temp sublabel-$$floor; done
--- src/main/scripts/make-tiles.sh
+++ src/main/scripts/make-tiles.sh
@@ -9,14 +9,6 @@ PREFIX=$3
 SVGFILE=$PREFIX-$FLOOR-$GENERATE_OPS.svg
 
 echo "Generating SVG for floor $FLOOR: $GENERATE_OPS"
-perl generate-tiles.pl $FLOOR $GENERATE_OPS > temp.svg
-touch $SVGFILE
-if `diff $SVGFILE temp.svg >/dev/null 2>&1`; then
-    echo "File has not changed"
-    rm temp.svg
-else
-    echo "File has changed"
-    mkdir -p tile
-    python svgToTile.py temp.svg temp tile/$PREFIX-$FLOOR
-    mv temp.svg $SVGFILE
-fi
+perl generate-tiles.pl $FLOOR $GENERATE_OPS > $SVGFILE
+mkdir -p tile
+python svgToTile.py $SVGFILE temp tile/$PREFIX-$FLOOR
