50798e08a0acdf281a351c690de53a274cc20d43
Andrew Rice <acr31@cam.ac.uk>
2013-03-03 12:33:41 +0000

    Added an extra line of progress information to the tile generation stage

--- src/main/scripts/make-tiles.sh
+++ src/main/scripts/make-tiles.sh
@@ -11,4 +11,5 @@ SVGFILE=$PREFIX-$FLOOR-$GENERATE_OPS.svg
 echo "Generating SVG for floor $FLOOR: $GENERATE_OPS"
 perl generate-tiles.pl $FLOOR $GENERATE_OPS > $SVGFILE
 mkdir -p tile
+echo "Generating tiles from SVG"
 python svgToTile.py $SVGFILE temp tile/$PREFIX-$FLOOR
