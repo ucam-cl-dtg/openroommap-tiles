5b2f8872ee53ba33090854677d8fbc9bf9a403d5
Andrew Rice <andy@animol.co.uk>
2015-01-06 16:55:05 +0000

    removed old scripts

--- src/main/scripts/old/backup-scripts/backup-machineroom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ssh open-room-map.dtg.cl.cam.ac.uk -f -L 5433:localhost:5432 sleep 10
-export PGPASSWORD=machineroom
-pg_dump -p 5433 -h localhost -U machineroom machineroom > machineroom-`date +%Y-%m-%d-%H:%M:%S`.sql
-export PGPASSWORD=
-
-
